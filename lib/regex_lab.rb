6f299309357702106cb9c75d684169e638643a78
Scott Hall <scotthall127@gmail.com>
2018-06-23 20:49:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/[aieou]+\w/)
-    true
-  else word.scan(/[bcdfghjklmnpqrstvwxyz]+\w/)
-    false
-  end
+  true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
