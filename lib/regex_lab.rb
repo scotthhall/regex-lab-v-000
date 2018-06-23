c49643c30f70c68d415660e556bb126c5d1ff444
Scott Hall <scotthall127@gmail.com>
2018-06-23 20:47:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  text.match(/[aieou]+\w/)
+  if word.match(/[aieou]+\w/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
